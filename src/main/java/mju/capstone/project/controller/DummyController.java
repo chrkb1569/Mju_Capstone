ce1a0e6373e589d32b2c09b4f90d20011e4b3e12
minsub <chrkb1569@naver.com>
2023-03-14 23:41:13 +0900

    feat : Add @ResponseStatus Annotation

--- src/main/java/mju/capstone/project/controller/DummyController.java
+++ src/main/java/mju/capstone/project/controller/DummyController.java
@@ -3,8 +3,10 @@ package mju.capstone.project.controller;
 import lombok.RequiredArgsConstructor;
 import mju.capstone.project.response.Response;
 import mju.capstone.project.service.DummyService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,11 +16,13 @@ public class DummyController {
     private final DummyService dummyService;
 
     @GetMapping("/all")
+    @ResponseStatus(HttpStatus.OK)
     public Response findAll() {
         return Response.success(dummyService.getList());
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Response findOne(@PathVariable Long id) {
         return Response.success(dummyService.getDummy(id));
     }
