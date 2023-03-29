c7b88466f3544ff8d7044f90908a0cd39c45e845
minsub <chrkb1569@naver.com>
2023-03-29 21:31:38 +0900

    feat : Add Image DTO Files

---
+++ src/main/java/mju/capstone/project/exception/image/ImageUploadFailureException.java
@@ -0,0 +1,8 @@
+package mju.capstone.project.exception.image;
+
+public class ImageUploadFailureException extends RuntimeException {
+
+    public ImageUploadFailureException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ src/main/java/mju/capstone/project/exception/image/NotSupportedExtensionException.java
@@ -0,0 +1,8 @@
+package mju.capstone.project.exception.image;
+
+public class NotSupportedExtensionException extends RuntimeException {
+
+    public NotSupportedExtensionException(String message) {
+        super(message);
+    }
+}
