dab6eb5680ab9d6506de50b2f2a9d3fdd7190193
bongj9 <lollypark501@naver.com>
2023-04-12 22:05:30 +0900

    login

--- src/main/java/mju/capstone/project/repository/DummyRepository.java
+++
@@ -1,7 +0,0 @@
-package mju.capstone.project.repository;
-
-import mju.capstone.project.domain.Dummy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DummyRepository extends JpaRepository<Dummy, Long> {
-}
