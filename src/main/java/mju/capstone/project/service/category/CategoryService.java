3248aee68b7d2bf7a2c298ac1c6e0f6b4a7b292c
minsub <chrkb1569@naver.com>
2023-05-01 14:15:26 +0900

    feat : Add Detailed Category Function

--- src/main/java/mju/capstone/project/service/category/CategoryService.java
+++ src/main/java/mju/capstone/project/service/category/CategoryService.java
@@ -2,11 +2,15 @@ package mju.capstone.project.service.category;
 
 import lombok.RequiredArgsConstructor;
 import mju.capstone.project.domain.category.Category;
+import mju.capstone.project.domain.category.LeafCategory;
+import mju.capstone.project.domain.category.SubCategory;
 import mju.capstone.project.dto.category.CategoryCreateDto;
 import mju.capstone.project.dto.category.CategoryResponseDto;
 import mju.capstone.project.exception.category.CategoryListEmptyException;
 import mju.capstone.project.exception.category.CategoryNotFoundException;
 import mju.capstone.project.repository.category.CategoryRepository;
+import mju.capstone.project.repository.category.LeafCategoryRepository;
+import mju.capstone.project.repository.category.SubCategoryRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,17 +22,42 @@ import java.util.stream.Collectors;
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
+    private final SubCategoryRepository subCategoryRepository;
+    private final LeafCategoryRepository leafCategoryRepository;
 
     // 카테고리 전체 조회
     @Transactional(readOnly = true)
     public List<CategoryResponseDto> findAll() {
-
         List<Category> findCategories = categoryRepository.findAll();
 
         if(findCategories.isEmpty()) throw new CategoryListEmptyException();
 
         return findCategories.stream()
                 .map(category -> new CategoryResponseDto().toDto(category)).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public List<CategoryResponseDto> findSubCategories(long categoryId) {
+        Category findItem = categoryRepository.findById(categoryId).orElseThrow();
+
+        List<SubCategory> findList = subCategoryRepository.findSubCategoriesByCategory(findItem);
+
+        if(findList.isEmpty()) throw new CategoryListEmptyException();
+
+        return findList.stream()
+                .map(value -> new CategoryResponseDto().toDto(value)).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public List<CategoryResponseDto> findLeafCategories(long categoryId, long subCategoryId) {
+        SubCategory findItem = subCategoryRepository.findById(subCategoryId).orElseThrow();
+
+        List<LeafCategory> findList = leafCategoryRepository.findLeafCategoriesBySubCategory(findItem);
+
+        if(findList.isEmpty()) throw new CategoryListEmptyException();
+
+        return findList.stream()
+                .map(value -> new CategoryResponseDto().toDto(value)).collect(Collectors.toList());
     }
 
     // 카테고리 생성
