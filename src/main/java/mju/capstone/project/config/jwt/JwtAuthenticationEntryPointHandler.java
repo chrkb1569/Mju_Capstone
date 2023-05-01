0b64565573ee1cac0836ab30482e6db3818974e0
minsub <chrkb1569@naver.com>
2023-05-01 14:00:28 +0900

    feat : Add JWT Handler Files

---
+++ src/main/java/mju/capstone/project/config/jwt/JwtAuthenticationEntryPointHandler.java
@@ -0,0 +1,19 @@
+package mju.capstone.project.config.jwt;
+
+import org.apache.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPointHandler implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpStatus.SC_UNAUTHORIZED);
+    }
+}
