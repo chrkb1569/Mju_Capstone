73ad7617dd51a2d7f805b072dc75f018e74dd595
minsub <chrkb1569@naver.com>
2023-05-01 14:11:54 +0900

    feat : Add OAuth2 Service

---
+++ src/main/java/mju/capstone/project/config/oauth2/OAuth2Service.java
@@ -0,0 +1,73 @@
+package mju.capstone.project.config.oauth2;
+
+import lombok.RequiredArgsConstructor;
+import mju.capstone.project.domain.user.OAuthAttributes;
+import mju.capstone.project.dto.oauth2.UserProfile;
+import mju.capstone.project.repository.user.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+@RequiredArgsConstructor
+public class OAuth2Service implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService oAuth2UserService = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = oAuth2UserService.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId(); // 로그인을 수행한 서비스의 이름
+
+        String userNameAttributeName = userRequest
+                .getClientRegistration()
+                .getProviderDetails()
+                .getUserInfoEndpoint()
+                .getUserNameAttributeName(); // PK가 되는 정보
+
+        Map<String, Object> attributes = oAuth2User.getAttributes(); // 사용자가 가지고 있는 정보
+
+        UserProfile userProfile = OAuthAttributes.extract(registrationId, attributes);
+        userProfile.setProvider(registrationId);
+
+        saveUser(userProfile);
+
+        Map<String, Object> customAttribute =
+                getCustomAttribute(registrationId, userNameAttributeName, attributes, userProfile);
+
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")),
+                customAttribute,
+                userNameAttributeName);
+    }
+
+    public Map getCustomAttribute(String registrationId,
+                                  String userNameAttributeName,
+                                  Map<String, Object> attributes,
+                                  UserProfile userProfile) {
+        Map<String, Object> customAttribute = new ConcurrentHashMap<>();
+
+        customAttribute.put(userNameAttributeName, attributes.get(userNameAttributeName));
+        customAttribute.put("provider", registrationId);
+        customAttribute.put("name", userProfile.getName());
+        customAttribute.put("email", userProfile.getEmail());
+
+        return customAttribute;
+    }
+
+    public void saveUser(UserProfile userProfile) {
+        if(!userRepository.existsUserByEmailAndProvider(userProfile.getEmail(), userProfile.getProvider()))
+            userRepository.save(userProfile.makeUser());
+    }
+}
